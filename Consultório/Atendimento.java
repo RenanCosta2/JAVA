b6c0a436b87d2884741b4d8d8f54b6169c9818c9
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-04-04 12:55:12 -0300

    Update Atendimento.java

--- Consultório/Atendimento.java
+++ Consultório/Atendimento.java
@@ -1,12 +1,69 @@
-package Consultório;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Atendimento {
     public static void main(String[] args) {
+        ArrayList<Paciente> pacientes = new ArrayList<>();
         LinkedList<Paciente> pacientesComuns = new LinkedList<>();
         PriorityQueue<Paciente> pacientesIdosos = new PriorityQueue<>();
+        int minutos = 1;
+        Random random = new Random();
+
+        while (true) {
+
+            if (minutos % 4 == 0 && pacientes.size() != 20) {
+                int[] rg = new int[9];
+                for (int i = 0; i < 8; i++) {
+                    rg[i] = random.nextInt(10);
+                }
+                rg[8] = calcularDigitoVerificadorRG(rg);
+                String rgString = Arrays.toString(rg);
+                int idade = random.nextInt(100);
+
+                Paciente paciente = new Paciente(rgString, idade);
+
+                pacientes.add(paciente);
+
+                if (paciente.getIdade() > 60) {
+                    pacientesIdosos.add(paciente);
+                } else{
+                    pacientesComuns.add(paciente);
+                }
+
+                System.out.println("\nPaciente " + paciente.toString() + " na fila...");
+            }
+
+            System.out.println("\nPacientes na fila: ");
+
+            for (Paciente paciente : pacientesIdosos) {
+                System.out.println(paciente);
+                
+            }
+            for (Paciente paciente : pacientesComuns) {
+                System.out.println(paciente);
+                
+            }
+
+            if(minutos % 5 == 0){
+                if (!pacientesIdosos.isEmpty()) {
+                    System.out.println("\n" + pacientesIdosos.poll() + " está sendo atendido...");
+                } else if(!pacientesComuns.isEmpty()){
+                    System.out.println("\n" + pacientesComuns.pollFirst() + " está sendo atendido...");
+                }
+            } 
+
+            minutos++;
+
+            if (pacientes.size() == 20 && pacientesComuns.isEmpty() && pacientesIdosos.isEmpty()) {
+                minutos += 5;
+                System.out.println("Consultório fechado!");
+                break;
+            }
+            
+        }
     }
 
     public static int calcularDigitoVerificadorRG(int[] rg){
