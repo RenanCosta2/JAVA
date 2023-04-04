e934a0a180b62099e5ab9c1fb0a757bdd5113346
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-04-04 12:55:29 -0300

    Update Paciente.java

--- Consultório/Paciente.java
+++ Consultório/Paciente.java
@@ -1,12 +1,28 @@
-package Consultório;
-
-public class Paciente {
+public class Paciente implements Comparable<Paciente> {
     private String rg;
     private int idade;
     
     public Paciente(String rg, int idade) {
         this.rg = rg;
         this.idade = idade;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    
+    @Override
+    public int compareTo(Paciente outro) {
+        return Integer.compare(idade, outro.idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente [rg=" + rg + ", idade=" + idade + "]";
     }
     
 }
